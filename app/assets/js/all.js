64f4f80fb41699e1971ba27a4fc2d462c251f990
Spm-Eng <dlspm0512@gmail.com>
2021-08-13 19:16:41 +0800

    feat: editor

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,26 @@
+// 文章編輯器
+ClassicEditor
+  .create( document.querySelector( '#editor' ), {
+    // 這裡可以設定 plugin
+    placeholder: 'Reply an answer…',
+  })
+  .catch( error => {
+      console.error( error );
+  } );
+
 $(function() {
-  console.log('Hello Bootstrap5');
+  $('.js_ckeditor_content').hide();
+
+  // 開啟編輯
+  $('.js-editor-open').on('click', function () {
+    $('.js_reply_content').slideUp();
+    $('.js_ckeditor_content').slideDown();
+    $('.js_operation_btns').toggleClass('d-none');
+  });
+  // 關閉編輯
+  $('.js-editor-close').on('click', function () {
+    $('.js_reply_content').slideDown();
+    $('.js_ckeditor_content').slideUp();
+    $('.js_operation_btns').toggleClass('d-none');
+  });
 });
