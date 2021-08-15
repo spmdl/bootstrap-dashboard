c885eaedd8c9f20c8d0cc1d28bd1d1ffc77ef138
Spm-Eng <dlspm0512@gmail.com>
2021-08-15 16:45:57 +0800

    Update 2021-08-15T08:45:51.545Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,26 +1,49 @@
 "use strict";
 
-// 文章編輯器
 ClassicEditor.create(document.querySelector('#editor'), {
-  // 這裡可以設定 plugin
-  placeholder: 'Reply an answer…'
-}) // .catch( error => {
-//     console.error( error );
-// } );
-;
-$(function () {
-  $('.js_ckeditor_content').hide(); // 開啟編輯
-
-  $('.js-editor-open').on('click', function () {
-    $('.js_reply_content').slideUp();
-    $('.js_ckeditor_content').slideDown();
-    $('.js_operation_btns').toggleClass('d-none');
-  }); // 關閉編輯
-
-  $('.js-editor-close').on('click', function () {
-    $('.js_reply_content').slideDown();
-    $('.js_ckeditor_content').slideUp();
-    $('.js_operation_btns').toggleClass('d-none');
-  });
+  toolbar: ['heading', 'bold', 'italic', 'bulletedList', 'numberedList', 'imageUpload', 'link', 'code', '|', 'outdent', 'indent'],
+  placeholder: 'Reply an answer…',
+  heading: [{
+    model: 'paragraph',
+    title: 'Paragraph',
+    "class": 'ck-heading_paragraph'
+  }, {
+    model: 'heading1',
+    view: 'h2',
+    title: 'Heading 1',
+    "class": 'ck-heading_heading1'
+  }, {
+    model: 'heading2',
+    view: 'h3',
+    title: 'Heading 2',
+    "class": 'ck-heading_heading2_fancy',
+    converterPriority: 'high'
+  }]
+}).then(function (editor) {
+  console.log(editor);
+})["catch"](function (error) {
+  console.error(error);
 });
+
+(function () {
+  var card = document.querySelector(".card");
+  var openBtn = document.querySelector(".js-Reply");
+  var cancelBtn = document.querySelector(".js-cancel");
+  var replyGroupOuter = document.querySelector(".replyGroup-outer");
+  var operationBtn = document.querySelector(".js-operation-btns");
+  openBtn.addEventListener("click", function (e) {
+    operationBtn.classList.add('d-none');
+    card.classList.remove('rounded');
+    card.classList.add('border-bottom-0', 'rounded-top');
+    replyGroupOuter.classList.add('active');
+    console.log('add active');
+  });
+  cancelBtn.addEventListener("click", function (e) {
+    card.classList.remove('border-bottom-0', 'rounded-top');
+    card.classList.add('rounded');
+    replyGroupOuter.classList.remove('active');
+    operationBtn.classList.remove('d-none');
+    console.log('remove active');
+  });
+})();
 //# sourceMappingURL=all.js.map
