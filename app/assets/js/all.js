068cfae68263e9caa482660c6f03615345f46c54
Spm-Eng <dlspm0512@gmail.com>
2021-08-15 16:45:06 +0800

    feat: editor

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,26 +1,56 @@
-// 文章編輯器
 ClassicEditor
   .create( document.querySelector( '#editor' ), {
-    // 這裡可以設定 plugin
+    toolbar: [ 'heading', 'bold', 'italic', 'bulletedList', 'numberedList',  'imageUpload', 'link', 'code', '|', 'outdent','indent' ],
     placeholder: 'Reply an answer…',
+    heading: [
+      {
+        model: 'paragraph',
+        title: 'Paragraph',
+        class: 'ck-heading_paragraph'
+      },
+      {
+        model: 'heading1',
+        view: 'h2',
+        title: 'Heading 1',
+        class: 'ck-heading_heading1'
+      },
+      {
+        model: 'heading2',
+        view: 'h3',
+        title: 'Heading 2',
+        class: 'ck-heading_heading2_fancy',
+        converterPriority: 'high'
+      }
+    ],
   })
+  .then( editor => {
+    console.log( editor );
+  } )
   .catch( error => {
-      console.error( error );
+    console.error( error );
   } );
 
-$(function() {
-  $('.js_ckeditor_content').hide();
+(function () {
+  let card = document.querySelector(".card");
+  let openBtn = document.querySelector(".js-Reply");
+  let cancelBtn = document.querySelector(".js-cancel");
+  let replyGroupOuter = document.querySelector(".replyGroup-outer");
+  let operationBtn = document.querySelector(".js-operation-btns");
 
-  // 開啟編輯
-  $('.js-editor-open').on('click', function () {
-    $('.js_reply_content').slideUp();
-    $('.js_ckeditor_content').slideDown();
-    $('.js_operation_btns').toggleClass('d-none');
+  openBtn.addEventListener("click", function (e) {
+    operationBtn.classList.add('d-none');
+    card.classList.remove('rounded');
+    card.classList.add('border-bottom-0', 'rounded-top');
+    replyGroupOuter.classList.add('active');
+    console.log('add active');
   });
-  // 關閉編輯
-  $('.js-editor-close').on('click', function () {
-    $('.js_reply_content').slideDown();
-    $('.js_ckeditor_content').slideUp();
-    $('.js_operation_btns').toggleClass('d-none');
+  cancelBtn.addEventListener("click", function (e) {
+    card.classList.remove('border-bottom-0', 'rounded-top');
+    card.classList.add('rounded');
+    replyGroupOuter.classList.remove('active');
+    operationBtn.classList.remove('d-none');
+    console.log('remove active');
   });
-});
+  
+
+}());
